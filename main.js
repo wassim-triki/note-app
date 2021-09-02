21e6803dc6237f81f5ea9ba79b7d315875f59a0c
Wassim Triki <wsmtriki@gmail.com>
2021-09-02 23:54:32 +0100

    fixed bug

--- main.js
+++ main.js
@@ -16,7 +16,6 @@ const work = new Tag("Work");
 const untagged = new Tag("Untagged");
 untagged.icon = '<i id="empty" class="far fa-bookmark"></i>';
 const tagList = [travel, personal, life, work, untagged];
-<<<<<<< HEAD
 
 appendNoteTypes(noteTypeList);
 appendTags(tagList);
@@ -30,7 +29,3 @@ navToggle.addEventListener("click", () => {
 navShrink.addEventListener("click", () => {
   navbar.classList.remove("slide-in");
 });
-=======
-const allNoteTypes = [...noteTypeList, ...tagList];
-appendNotesToNav(allNoteTypes);
->>>>>>> 8d94809 (add colored icons procedurally)
